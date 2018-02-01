441f0aff6a960719530ef124e6bba802a7e7b252
Maciej Winnicki <mthenw@users.noreply.github.com>
2018-02-01 13:41:11 +0100

    simplify code a bit (#7)

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,7 +1,5 @@
-'use script';
+"use script";
 
 module.exports.handler = (event, context, cb) => {
-    console.log('Received event', event);
-
-    cb(null, { message: 'success' });
+  cb(null, { body: "success" });
 };
--- src/index.js
+++ src/index.js
@@ -1,194 +1,236 @@
-'use strict';
+"use strict";
 
-// Try to remove this. Such a large package
-const _ = require('lodash');
-const fdk = require('@serverless/fdk');
-const crypto = require('crypto');
+const merge = require("lodash.merge");
+const fdk = require("@serverless/fdk");
+const crypto = require("crypto");
+const chalk = require("chalk");
 
 class EGPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
 
-    this.awsProvider = this.serverless.getProvider('aws');
-    this.providerNaming = this.awsProvider.naming;
+    this.awsProvider = this.serverless.getProvider("aws");
 
     this.hooks = {
-      'package:compileEvents': this.compile.bind(this),
-      'after:deploy:deploy': this.afterDeploy.bind(this),
+      "package:compileEvents": this.addUserDefinition.bind(this),
+      "after:deploy:finalize": this.configureEventGateway.bind(this)
     };
   }
 
   getConfig() {
-    return this.serverless.service.custom && this.serverless.service.custom.eventgateway;
+    if (
+      this.serverless.service.custom &&
+      this.serverless.service.custom.eventgateway
+    ) {
+      const config = this.serverless.service.custom.eventgateway;
+      config.eventsAPI = `https://${config.subdomain}.eventgateway-dev.io`;
+      config.configurationAPI = "https://config.eventgateway-dev.io/";
+      return config;
+    }
+
+    return null;
+  }
+
+  configureEventGateway() {
+    const config = this.getConfig();
+    if (!config) {
+      throw new Error(
+        "No Event Gateway configuration provided in serverless.yaml"
+      );
+    }
+
+    if (!config.subdomain) {
+      throw new Error(
+        'Required "subdomain" property is missing from Event Gateway configuration provided in serverless.yaml'
+      );
+    }
+
+    if (!config.apikey) {
+      throw new Error(
+        'Required "apikey" property is missing from Event Gateway configuration provided in serverless.yaml'
+      );
+    }
+
+    this.eventGateway = fdk.eventGateway({
+      url: config.eventsAPI,
+      configurationUrl: config.configurationAPI
+    });
+
+    this.serverless.cli.consoleLog("");
+    this.serverless.cli.consoleLog(
+      chalk.yellow.underline("Event Gateway Plugin")
+    );
+
+    return this.awsProvider
+      .request(
+        "CloudFormation",
+        "describeStacks",
+        { StackName: this.awsProvider.naming.getStackName() },
+        this.awsProvider.getStage(),
+        this.awsProvider.getRegion()
+      )
+      .then(data => {
+        const stack = data.Stacks.pop();
+        if (!stack) {
+          throw new Error("Unable to fetch CloudFormation stack information");
+        }
+
+        const outputs = this.parseOutputs(stack);
+
+        if (
+          !outputs.EventGatewayUserAccessKey ||
+          !outputs.EventGatewayUserSecretKey
+        ) {
+          throw new Error(
+            "Event Gateway Access Key or Secret Key not found in outputs"
+          );
+        }
+
+        process.env.EVENT_GATEWAY_TOKEN = config.apikey;
+
+        this.filterFunctionsWithEvents().map(name => {
+          const outputKey = this.awsProvider.naming.getLambdaVersionOutputLogicalId(
+            name
+          );
+          const arn = outputs[outputKey];
+          const functionId = crypto
+            .createHash("sha256", "utf8")
+            .update(arn)
+            .digest("hex");
+
+          this.eventGateway
+            .registerFunction({
+              functionId: functionId,
+              provider: {
+                type: "awslambda",
+                arn: arn,
+                region: this.awsProvider.getRegion(),
+                awsAccessKeyId: outputs.EventGatewayUserAccessKey,
+                awsSecretAccessKey: outputs.EventGatewayUserSecretKey
+              }
+            })
+            .then(() => {
+              this.serverless.cli.consoleLog(
+                `EventGateway: Function "${name}" registered.`
+              );
+
+              const func = this.serverless.service.getFunction(name);
+              func.events.forEach(functionEvent => {
+                if (!functionEvent.eventgateway) return;
+
+                const event = functionEvent.eventgateway;
+                if (!event.event || !event.path) return;
+
+                const path =
+                  (event.path.startsWith("/") ? "" : "/") + event.path;
+                const subscribeEvent = {
+                  functionId,
+                  event: event.event,
+                  path: `/${config.subdomain}${path}`
+                };
+
+                if (event.event == "http") {
+                  subscribeEvent.method = event.method || "GET";
+                }
+
+                this.eventGateway.subscribe(subscribeEvent);
+
+                this.serverless.cli.consoleLog(
+                  `EventGateway: Function "${name}" subscribed to "${
+                    event.event
+                  }" event.`
+                );
+              });
+
+              this.serverless.cli.consoleLog("");
+              this.serverless.cli.consoleLog(
+                `EventGateway: Endpoint URL: ${config.eventsAPI}`
+              );
+            });
+        });
+      });
+  }
+
+  filterFunctionsWithEvents() {
+    return this.serverless.service
+      .getAllFunctions()
+      .reduce((agg, functionName) => {
+        const func = this.serverless.service.getFunction(functionName);
+        if (!func.events) {
+          return;
+        }
+        agg.push(functionName);
+        return agg;
+      }, []);
+  }
+
+  parseOutputs(stack) {
+    return stack.Outputs.reduce((agg, current) => {
+      if (current.OutputKey && current.OutputValue) {
+        agg[current.OutputKey] = current.OutputValue;
+      }
+      return agg;
+    }, {});
   }
 
   addUserDefinition() {
-    _.merge(this.serverless.service.provider.compiledCloudFormationTemplate.Resources, {
-      "EventGatewayUser": {
-        "Type": "AWS::IAM::User"
-      },
-      "EventGatewayUserPolicy": {
-        "Type": "AWS::IAM::ManagedPolicy",
-        "Properties": {
-          "Description": "This policy allows Custom plugin to gather data on IAM users",
-          "PolicyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
+    merge(
+      this.serverless.service.provider.compiledCloudFormationTemplate.Resources,
+      {
+        EventGatewayUser: {
+          Type: "AWS::IAM::User"
+        },
+        EventGatewayUserPolicy: {
+          Type: "AWS::IAM::ManagedPolicy",
+          Properties: {
+            Description:
+              "This policy allows Custom plugin to gather data on IAM users",
+            PolicyDocument: {
+              Version: "2012-10-17",
+              Statement: [
+                {
+                  Effect: "Allow",
+                  Action: ["lambda:InvokeFunction"],
+                  Resource: "*"
+                }
+              ]
+            },
+            Users: [
               {
-                "Effect": "Allow",
-                "Action": [
-                  "lambda:InvokeFunction",
-                ],
-                "Resource": "*"
+                Ref: "EventGatewayUser"
               }
             ]
-          },
-          "Users": [
-            {
-              "Ref": "EventGatewayUser"
+          }
+        },
+        EventGatewayUserKeys: {
+          Type: "AWS::IAM::AccessKey",
+          Properties: {
+            UserName: {
+              Ref: "EventGatewayUser"
             }
-          ]
-        }
-      },
-      "EventGatewayUserKeys": {
-        "Type": "AWS::IAM::AccessKey",
-        "Properties": {
-          "UserName": {
-            "Ref": "EventGatewayUser"
           }
         }
       }
-    })
+    );
 
-    _.merge(this.serverless.service.provider.compiledCloudFormationTemplate.Outputs, {
-      "EventGatewayUserAccessKey": {
-        "Value": {
-          "Ref": "EventGatewayUserKeys"
+    merge(
+      this.serverless.service.provider.compiledCloudFormationTemplate.Outputs,
+      {
+        EventGatewayUserAccessKey: {
+          Value: {
+            Ref: "EventGatewayUserKeys"
+          },
+          Description: "Access Key ID of Custom User"
         },
-        "Description": "Access Key ID of Custom User"
-      },
-      "EventGatewayUserSecretKey": {
-        "Value": {
-          "Fn::GetAtt": [
-            "EventGatewayUserKeys",
-            "SecretAccessKey"
-          ]
-        },
-        "Description": "Secret Key of Custom User"
-      }
-    })
-  }
-
-  compile() {
-    const config = this.getConfig();
-    if (!config) {
-      throw new Error('No EventGateway configuration provided in serverless.yaml')
-    }
-    if (!config['subdomain'] || !config['subdomain'].length) {
-      throw new Error('Required "subdomain" property is missing from EventGateway configuration provided in serverless.yaml')
-    }
-    this.config = config
-
-    this.eventGatewayURL = `https://${this.config['subdomain']}.eventgateway-dev.io`
-    this.eventGateway = fdk.eventGateway({
-      url: this.eventGatewayURL,
-      configurationUrl: 'https://config.eventgateway-dev.io/',
-    })
-
-    this.addUserDefinition()
-  }
-
-  afterDeploy() {
-    return this.awsProvider.request(
-      'CloudFormation',
-      'describeStacks',
-      { StackName: this.awsProvider.naming.getStackName() },
-      this.awsProvider.getStage(),
-      this.awsProvider.getRegion()
-    ).then(data => {
-      if (!(data instanceof Object && data['Stacks'] && data['Stacks'] instanceof Array)) {
-        throw new Error('Unable to fetch Stack information')
-      }
-
-      const stack = data['Stacks'].pop() || { Outputs: [] }
-      const outputs = stack.Outputs || []
-
-      const parsedOutputs = outputs.reduce((agg, current) => {
-        if (current['OutputKey'] && current['OutputValue']) {
-          agg[current['OutputKey']] = current['OutputValue']
+        EventGatewayUserSecretKey: {
+          Value: {
+            "Fn::GetAtt": ["EventGatewayUserKeys", "SecretAccessKey"]
+          },
+          Description: "Secret Key of Custom User"
         }
-        return agg
-      }, {})
-
-      if (!(parsedOutputs['EventGatewayUserAccessKey'] && parsedOutputs['EventGatewayUserSecretKey'])) {
-        throw new Error('Access Key or Secret Key not found in outputs')
       }
-
-      this.serverless.cli.log('EventGatewayUserAccessKey: ' + parsedOutputs['EventGatewayUserAccessKey'])
-      this.serverless.cli.log('EventGatewayUserSecretKey: ' + parsedOutputs['EventGatewayUserSecretKey'])
-
-      const functionsWithEvents = this.serverless.service.getAllFunctions().reduce((agg, functionName) => {
-        const functionObj = this.serverless.service.getFunction(functionName)
-
-        if (!(functionObj instanceof Object && functionObj['events'] instanceof Array && functionObj['events'].length)) {
-          return
-        }
-        agg.push(functionName.toLowerCase())
-
-        return agg
-      }, [])
-
-      Object.keys(parsedOutputs).forEach(key => {
-        const outputFunctionName = key.substring(0, key.indexOf('LambdaFunctionQualifiedArn')).toLowerCase()
-        const actualFunctionName = functionsWithEvents.find(thisfunc => thisfunc.toLowerCase() === outputFunctionName)
-        if (key.endsWith('LambdaFunctionQualifiedArn') && actualFunctionName) {
-          this.serverless.cli.log(key + ": " + parsedOutputs[key])
-          const functionId = crypto.createHash('sha256','utf8').update(parsedOutputs[key]).digest('hex')
-
-          this.eventGateway.registerFunction({
-            functionId: functionId,
-            provider: {
-              type: 'awslambda',
-              arn: parsedOutputs[key],
-              region: this.awsProvider.getRegion(),
-              awsAccessKeyId: parsedOutputs['EventGatewayUserAccessKey'],
-              awsSecretAccessKey: parsedOutputs['EventGatewayUserSecretKey'],
-            }
-          })
-
-          const functionObj = this.serverless.service.getFunction(actualFunctionName)
-          functionObj['events'].forEach(eventObj => {
-            if (!eventObj['eventgateway']) return
-
-            const event = eventObj['eventgateway']
-            if (!(
-              event instanceof Object &&
-              event['event'] &&
-              event['path']
-            )) {
-              return
-            }
-
-            const eventPath = (event['path'].startsWith('/') ? '' : '/') + event['path']
-            const eventId = event['event']
-            const subscribeEvent = {
-              // Validation of nested objects is above when iterating over all the functions
-              event: eventId,
-              functionId: functionId,
-              method: event['method'].toUpperCase(),
-              path: `/${this.config['subdomain']}${eventPath}`,
-            }
-            if (event['event'] === 'http') {
-              subscribeEvent['method'] = (event['method'] && event['method'].toUpperCase()) || 'GET'
-            }
-            this.eventGateway.subscribe(subscribeEvent)
-
-            this.serverless.cli.log(`${this.eventGatewayURL}${eventPath}`)
-          })
-        }
-      })
-    })
+    );
   }
 }
 
