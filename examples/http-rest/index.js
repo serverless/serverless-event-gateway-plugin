737a24b14914ec63175e8718d6fc1532210f9dba
Maciej Winnicki <mthenw@users.noreply.github.com>
2018-02-02 14:24:55 +0100

    add HTTP REST example (#12)
    * add rest example
    
    * update basic example
    
    * Add quickstart instructions to README (#9)
    
    * Add state persistence (#4)
    
    * Improve a check
    
    * Ignore .egstate.json
    
    * Ignore default.etcd
    
    * npm install in basic example
    
    * npm install in project
    
    * Add state persistence
    
    * Use property access instead of array access
    
    * Dont check for subscriptionId in subObj
    
    * Add semi's
    
    * Move API key configuration up
    
    * update package.json
    
    * update package.json
    
    * update package.json
    
    * update basic example
    
    * add HTTP REST example

---
+++ examples/http-rest/index.js
@@ -0,0 +1,16 @@
+"use script";
+
+module.exports.get = (event, context, cb) => {
+  console.log(event);
+  cb(null, { body: `GET resource with id: ${event.data.params.id}` });
+};
+
+module.exports.post = (event, context, cb) => {
+  console.log(event);
+  cb(null, { body: `POST resource with id: ${event.data.params.id}` });
+};
+
+module.exports.delete = (event, context, cb) => {
+  console.log(event);
+  cb(null, { body: `DELETE resource with id: ${event.data.params.id}` });
+};
